0aed303c3962bf475df6324128ed0b8a5727a2e5
Waqar Ahmad <engrwaqar37@gmail.com>
2022-06-10 18:18:01 +0500

    initial comment

---
+++ src/components/AddEmployee.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AddEmployee = () => {
+  return (
+    <div className='flex max-w-2xl'>
+        <div className='px-8 py-8'>
+            <div className=''>
+                <h1>Add New Employee</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddEmployee
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+        <div className='bg-gray-800'>
+            <div className='h-16 px-8 flex items-center'>
+                <p className='text-white font-bold'>Employee Management System</p>
+            </div>
+        </div>
+    )
+}
